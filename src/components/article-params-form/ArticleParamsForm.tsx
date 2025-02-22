7c370501ae296d319e279fcc0f1e7321e3397679
Vas Tregubov <vastregubov@gmail.com>
2025-02-22 21:19:40 +0300

    feat(articleparamsform): add toggle logic for ArticleParamsForm component
    - Implemented logic to toggle the visibility of the ArticleParamsForm.

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,23 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	isOpen: boolean;
+	onToggle: () => void;
+};
+
+export const ArticleParamsForm = ({
+	isOpen,
+	onToggle,
+}: ArticleParamsFormProps) => {
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={onToggle} />
+			<aside
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
